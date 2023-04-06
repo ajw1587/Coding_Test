b564347740fe5e3c2884d3febed59c4341545552
InWoo <dlsdnghkd0503@naver.com>
2023-04-06 10:56:35 +0900

    Test

--- Coding_Test/Coding_Test/96_Problem_가장_높은_탑_쌓기.cpp
+++ Coding_Test/Coding_Test/96_Problem_가장_높은_탑_쌓기.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 using namespace chrono;
@@ -15,12 +17,63 @@ using namespace chrono;
 // (조건3)벽돌들의 높이는 같을 수도 있다.
 // (조건4)탑을 쌓을 때 밑면이 좁은 벽돌 위에 밑면이 넓은 벽돌은 놓을 수 없다.
 // (조건5)무게가 무거운 벽돌을 무게가 가벼운 벽돌 위에 놓을 수 없다.
+//
+// 벽돌 밑면의 넓이, 벽돌의 높이 그리고 무게가 차례대로 양의 정수로 주어진다.
+
+struct my_block
+{
+	int area, height, weight;
+	my_block(int a, int h, int w)
+	{
+		area = a;
+		height = h;
+		weight = w;
+	}
+
+	bool operator<(const my_block c) const
+	{
+		return area > c.area;
+	}
+};
 
 int No96()
 {
 	system_clock::time_point start = system_clock::now();
 
+	int N = 5;
+	vector<int> res(N, 0);
+	// 넓이, 높이, 무게
+	vector<vector<int>> info =
+	{{ 25, 3, 4 },
+	{ 4, 4, 6 },
+	{ 9, 2, 3 },
+	{ 16, 2, 5 },
+	{ 1, 5, 2 }};
 
+	// 넓이 기준 내림차순 정렬
+	vector<my_block> list;
+	for (int i = 0; i < N; i++)
+	{
+		list.push_back(my_block(info[i][0], info[i][1], info[i][2]));
+	}
+	sort(list.begin(), list.end());
+
+	// 넓이 제외 진행해주기
+	res[0] = list[0].height;
+	for (int i = 1; i < res.size(); i++)
+	{
+		for (int j = i - 1; j >= 0; j--)
+		{
+			if (list[i].weight < list[j].weight &&  res[i] < res[j] + list[i].height)
+			{
+				res[i] = res[j] + list[i].height;
+			}
+			else
+			{
+				res[i] = list[i].height;
+			}
+		}
+	}
 
 	system_clock::time_point end = system_clock::now();
 	microseconds micro = duration_cast<microseconds>(end - start);
