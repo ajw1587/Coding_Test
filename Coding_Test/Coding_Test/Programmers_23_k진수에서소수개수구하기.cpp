3a6bf98f8dace52187f7c0a909ecd0cff808aad4
InWoo <dlsdnghkd0503@naver.com>
2023-05-08 23:32:50 +0900

    KAKAO

--- Coding_Test/Coding_Test/Programmers_23_k진수에서소수개수구하기.cpp
+++ Coding_Test/Coding_Test/Programmers_23_k진수에서소수개수구하기.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <string>
 
 using namespace std;
 using namespace chrono;
 
 // k진수에서 소수 개수 구하기
 // https://school.programmers.co.kr/learn/courses/30/lessons/92335
+
+string change_num(int n, int k)
+{
+    string tmp;
+    while (n)
+    {
+        tmp += to_string(n % k);
+        n /= k;
+    }
+
+    reverse(tmp.begin(), tmp.end());
+    return tmp;
+}
+
 int No()
 {
     system_clock::time_point start = system_clock::now();
@@ -14,8 +29,11 @@ int No()
     int n = 437674;
     int k = 3;
 
-    // k 진수로 바꿔주기
+    n = 10;
+    k = 2;
 
+    // k 진수로 바꿔주기
+    string num = change_num(n, k);
 
     // 0을 기준으로 값 나눠주기
 
