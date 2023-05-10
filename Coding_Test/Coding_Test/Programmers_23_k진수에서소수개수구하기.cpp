5261e049762a2779b15562653f571c54c2b4fd96
InWoo <dlsdnghkd0503@naver.com>
2023-05-11 00:24:08 +0900

    KAKAO k

--- Coding_Test/Coding_Test/Programmers_23_k진수에서소수개수구하기.cpp
+++ Coding_Test/Coding_Test/Programmers_23_k진수에서소수개수구하기.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 using namespace chrono;
+typedef long long ll;
 
 // k진수에서 소수 개수 구하기
 // https://school.programmers.co.kr/learn/courses/30/lessons/92335
@@ -29,38 +30,59 @@ vector<string> divide_num(string num)
 
     for (int i = 0; i < num.size(); i++)
     {
-        if (num[i] != '0')
+        if (num[i] == '0' && tmp != "")
+        {
+            if (tmp != "1")
+            {
+                out.push_back(tmp);
+            }
+            tmp = "";
+        }
+        else  // num[i] != '0'
         {
             tmp += num[i];
         }
-        else
-        {
-            if (tmp == "1")
-            {
-                tmp = "";
-            }
-            else
-            {
-                
-            }
-        }
     }
 
+    if (!tmp.empty()) out.push_back(tmp);
+
     return out;
+}
+
+bool check_answer(ll num)
+{
+    if (num < 2) return false;
+
+    for (int i = 2; i <= sqrt(num); i++)
+    {
+        if (num % i == 0) return false;
+    }
+
+    return true;
 }
 
 int No()
 {
     system_clock::time_point start = system_clock::now();
 
-    int n = 437674;
-    int k = 3;
+    int answer = 0;
+    //int n = 437674;
+    //int k = 3;
+    int n = 110011;
+    int k = 10;
 
     // k 진수로 바꿔주기
     string num = change_num(n, k);
 
     // 0을 기준으로 값 나눠주기
+    vector<string> divi_num = divide_num(num);
 
+    // 소수 판단하기
+    for (int i = 0; i < divi_num.size(); i++)
+    {
+        ll num = stol(divi_num[i]);
+        if (check_answer(num)) answer++;
+    }
 
     system_clock::time_point end = system_clock::now();
     microseconds micro = duration_cast<microseconds>(end - start);
