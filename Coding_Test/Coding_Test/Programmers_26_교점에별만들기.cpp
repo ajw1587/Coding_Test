1fc8dcbfb5321d56b2a2edcef5dae65b52d4ad38
InWoo <dlsdnghkd0503@naver.com>
2023-05-16 22:35:34 +0900

    star

--- Coding_Test/Coding_Test/Programmers_26_교점에별만들기.cpp
+++ Coding_Test/Coding_Test/Programmers_26_교점에별만들기.cpp
@@ -7,9 +7,15 @@ using namespace chrono;
 
 // 교점에 별 만들기
 // https://school.programmers.co.kr/learn/courses/30/lessons/87377
+// 어렵네...?
 int No()
 {
     system_clock::time_point start = system_clock::now();
+
+    vector<vector<int>> line =
+    { {2, -1, 4}, {-2, -1, 4}, {0, -1, 1}, {5, -8, -12}, {5, 8, 12} };
+
+    // 
 
     system_clock::time_point end = system_clock::now();
     microseconds micro = duration_cast<microseconds>(end - start);
