493226b9cb29b78a08ac411506e95233acbac2d7
InWoo <dlsdnghkd0503@naver.com>
2023-05-17 22:10:28 +0900

    Hard

--- Coding_Test/Coding_Test/Programmers_26_교점에별만들기.cpp
+++ Coding_Test/Coding_Test/Programmers_26_교점에별만들기.cpp
@@ -8,6 +8,15 @@ using namespace chrono;
 // 교점에 별 만들기
 // https://school.programmers.co.kr/learn/courses/30/lessons/87377
 // 어렵네...?
+
+// Ax + By + E = 0
+// Cx + Dy + F = 0
+void get_dot(vector<int> dot1, vector<int> dot2)
+{
+    int A = dot1[0], B = dot1[1], E = dot1[2];
+    int C = dot2[0], D = dot2[1], F = dot2[2];
+}
+
 int No()
 {
     system_clock::time_point start = system_clock::now();
